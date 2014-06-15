988b18bac6ea58aac720fdab60d5601330eb05b3
Evan Bleiweiss <evan.bleiweiss@gmail.com>
2014-06-14 20:28:20 -0500

    cleanup main file a little

--- web/scripts/main.dart
+++ web/scripts/main.dart
@@ -2,24 +2,14 @@ library awst;
 
 // main script for site
 import 'dart:html';
-//import 'dart:io';
-//import 'package:oauth2/oauth2.dart' as oauth2;
-
 import 'dart:async';
-
 import "package:json_object/json_object.dart";
+
 //part 'twitter.dart';
 
 void main() {
-  /* querySelector('#inputName').onInput.listen(updateBadge); */
   locateEvan("78756");
-//  getLatestTweet();
-//  new Uri.https('api.twitter.com', '/1.1/statuses/user_timeline.json?screenname=evanbleiweiss&count=1');
 }
-
-/* void updateBadge(Event e) { */
-/*   querySelector('#badgeName').text = (e.target as InputElement).value; */
-/* } */
 
 locateEvan(place) {
   var yqlUri = "http://query.yahooapis.com/v1/public/yql";
@@ -39,13 +29,13 @@ textInject(selector, msg) {
   querySelector('$selector').text = '$msg';
 }
 
- getLatestTweet() {
+getLatestTweet() {
   var host = "api.twitter.com";
   var path = "/1.1/statuses/user_timeline.json";
   var params = {'screen_name': 'evanbleiweiss', 'count': '1'};
   //  var uri = new Uri.http(host, path, params);
   var uri = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=evanbleiweiss&count=1";
-//  auth();
+  //  auth();
   HttpRequest.getString(uri).then( (String resp){
     querySelector('#lastTweet').text = resp;
    });
