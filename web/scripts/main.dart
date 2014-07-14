7c60817f0054497d297e5bf62b3a7486b8e158a8
Evan Bleiweiss <evan.bleiweiss@gmail.com>
2014-07-14 01:27:53 -0500

    rm twitter code
    there's something weird about the oauth implementation on twitter's end
    that makes it difficult to keep the secrets private when going browser
    only
    
    should probably use their web component

--- web/scripts/main.dart
+++ web/scripts/main.dart
@@ -5,8 +5,6 @@ import 'dart:html';
 import 'dart:async';
 import "package:json_object/json_object.dart";
 
-//part 'twitter.dart';
-
 void main() {
   locateEvan("78756");
 }
@@ -29,14 +27,3 @@ textInject(selector, msg) {
   querySelector('$selector').text = '$msg';
 }
 
-getLatestTweet() {
-  var host = "api.twitter.com";
-  var path = "/1.1/statuses/user_timeline.json";
-  var params = {'screen_name': 'evanbleiweiss', 'count': '1'};
-  //  var uri = new Uri.http(host, path, params);
-  var uri = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=evanbleiweiss&count=1";
-  //  auth();
-  HttpRequest.getString(uri).then( (String resp){
-    querySelector('#lastTweet').text = resp;
-   });
- }
