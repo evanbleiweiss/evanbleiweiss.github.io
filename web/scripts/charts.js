a78f3f4b7b9b892a35abb07cf017366077493dca
Evan Bleiweiss <evan.bleiweiss@gmail.com>
2014-08-11 20:50:35 -0500

    restructure js code for modularity

--- web/scripts/charts.js
+++ web/scripts/charts.js
@@ -1,12 +1,20 @@
 // Playing around with d3
 
-var canvas = d3
-  .select('#svgbox')
-  .append('svg:svg')
-  .attr('width', 400)
-  .attr('height', 300)
-  .append('svg:rect')
-  .attr('x', 100)
-  .attr('y', 100)
-  .attr('height', 100)
-  .attr('width', 200);
+var foodScores = function() {
+  var foodScoresUri = 'http://data.austintexas.gov/resource/ecmv-9xxi.json';
+
+  var canvas = d3
+    .select('#svgbox')
+    .append('svg:svg')
+    .attr('width', 400)
+    .attr('height', 300)
+    .append('svg:rect')
+    .attr('x', 100)
+    .attr('y', 100)
+    .attr('height', 100)
+    .attr('width', 200);
+
+  return canvas;
+}
+
+foodScores();
