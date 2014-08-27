9e3c4d5dc7fd2f14d3231912705b821f7d45207e
Evan Bleiweiss <evan.bleiweiss@gmail.com>
2014-08-27 00:15:46 -0500

    fix the scales. mvp
    currently the highest scores are at the bottom of the svg

--- web/scripts/charts.js
+++ web/scripts/charts.js
@@ -2,59 +2,39 @@
 
 var foodScores = function() {
   var data;
-  var foodScoresUri = 'http://data.austintexas.gov/resource/ecmv-9xxi.json?zip_code=78756&restaurant_name=banzai'; //Use Socrata (SODA) API endpoint to get data
+  var foodScoresUri = 'http://data.austintexas.gov/resource/ecmv-9xxi.json?zip_code=78756'; //Use Socrata (SODA) API endpoint to get data
 
-  var xRange = d3.time.scale().range([0, height]).domain([new Date(2007, 01, 01), new Date(2015, 01, 01)]);
-  // var xRange = d3.scale.linear().range ([20, 180]).domain([0, 300]);
-  var yRange = d3.scale.linear().range ([20, 180]).domain([0, 300]);
-
-// EXPERIMENT
   var width = 500,
       height = 500,
       margin = 50;
-  var x=d3.scale.linear().domain([0,100]).range([margin,width-margin]); //TODO: Make this a time scale - https://github.com/mbostock/d3/wiki/Time-Scales
-  var y=d3.scale.linear().domain([0,100]).range([height-margin,margin]);
-  var r=d3.scale.linear().domain([0,100]).range([0,20]);
+  var xRange = d3.time.scale().range([margin,width-margin]).domain([new Date(2010, 01, 01), new Date(2015, 01, 01)]);
+  var yRange = d3.scale.linear().range([margin, height-margin]).domain([0, 100]);
+  var r=d3.scale.linear().domain([50,200]).range([0,20]);
   
   // Select elements
   var svg = d3
     .select('#svgbox')
     .append('svg:svg')
     .attr('width', width)
-    .attr('height', height)
-    // .append('svg:rect') // and draw a box
-    // .attr('x', 100)
-    // .attr('y', 100)
-    // .attr('height', 300)
-    // .attr('width', 400);
-// EOE
-
-  function dataMuncher(data) {
-    svg.selectAll("circle").data(data).enter()
-    .append("circle")
-    .attr("cx", function(d) {return xRange(Date(+d.inspection_date));})
-    .attr("cy", function(d) {return yRange(+d.score);})
-    .attr("r",function(d) {return r(100);})
-      .append("title")
-      .text(function(d) {return d.restaurant_name;});
-  }
+    .attr('height', height);
 
   function makeCircles(data) {
     svg.selectAll("circle").data(data).enter()
     .append("circle")
-    .attr("cx", function(d) {return xRange(+d.score);})
+    .attr("cx", function(d) {return xRange(humanTime(+d.inspection_date));})
     .attr("cy", function(d) {return yRange(+d.score);})
     .attr("r",function(d) {return r(+d.score);})
-      .append("title")
-      .text(function(d) {return d.restaurant_name;});
+    .append("title")
+    .text(function(d) {return d.restaurant_name;});
   }
 
   //Utility for converting (partial) epoch time to human format
-  //might use moment.js if this grows too big
+  //might use moment.js if this grows
   function humanTime(input) {
     return new Date(parseFloat(input) * 1000);
   }
   
+  // Kick everything off -- 
   // remote request can be written as
   // d3.json(uri, callback);
   //          OR
