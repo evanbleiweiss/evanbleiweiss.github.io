f0c7f3ab2a249c50b30ffc7588f574b30eb229f7
Evan Bleiweiss <evan.bleiweiss@gmail.com>
2014-09-04 00:59:00 -0500

    use a smaller data set for testing. data is appended to svg elem

--- web/scripts/charts.js
+++ web/scripts/charts.js
@@ -2,15 +2,13 @@
 
 var foodScores = function() {
   var data;
-  var foodScoresUri = 'http://data.austintexas.gov/resource/ecmv-9xxi.json?zip_code=78756'; //Use Socrata (SODA) API endpoint to get data
+  var foodScoresUri = 'http://data.austintexas.gov/resource/ecmv-9xxi.json?zip_code=78756&restaurant_name=uchiko'; //Use Socrata (SODA) API endpoint to get data
 
   var width = 500,
       height = 250,
       margin = 50;
   var xRange = d3.time.scale().range([margin,width-margin]).domain([new Date(2011, 08, 15), new Date(2015, 01, 01)]);
   var yRange = d3.scale.linear().range([height-margin, margin]).domain([77, 100]);
-  var r=d3.scale.linear().domain([50,200]).range([0,20]);
-  
   // Select elements
   var svg = d3
     .select('#svgbox')
@@ -19,6 +17,8 @@ var foodScores = function() {
     .attr('height', height);
 
   function makeCircles(data) {
+    var r=d3.scale.linear().domain([50,200]).range([0,20]);
+
     svg.selectAll("circle").data(data).enter()
     .append("circle")
     .attr("cx", function(d) {return xRange(humanTime(+d.inspection_date));})
@@ -26,6 +26,17 @@ var foodScores = function() {
     .attr("r",function(d) {return r(+d.score);})
     .append("title")
     .text(function(d) {return d.restaurant_name +" "+ d.score +" "+ humanTime(+d.inspection_date);});
+  }
+
+  function makeLines(data) {
+    var line = d3.svg.line()
+    .x(function(d) { return xRange(humanTime(+d.inspection_date)); })
+    .y(function(d) { return yRange(+d.score); });    
+  
+    svg.selectAll('svg').data(data).enter().append("path")
+    
+    .attr("class", "line")
+    .attr("d", line);
   }
 
   //Utility for converting (partial) epoch time to human format
@@ -44,8 +55,9 @@ var foodScores = function() {
   // .on("load", function(json){
   //   PROCESS DATA HERE
   // })
-  var dataGrabbler = d3.json(foodScoresUri, makeCircles);
+  var dataGrabbler = d3.json(foodScoresUri, makeLines);
   
+  // makeLines(data);
 }
 
 foodScores();
